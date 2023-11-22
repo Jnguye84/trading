77f3d9f71116cc42bfd17a93a820566dbbdf6d91
Jessica Nguyen <89553284+Jnguye84@users.noreply.github.com>
2023-11-22 16:17:23 -0500

    updated dataframe to have info now in websearch.py

--- virtenv/websearch.py
+++ virtenv/websearch.py
@@ -2,39 +2,81 @@ from bs4 import BeautifulSoup
 from bs4.element import Comment
 from urllib.request import Request, urlopen
 import nltk
-from bert import sentiment
+from webcrawl_api import lst
+import pandas as pd
 # nltk.download('punkt')
 
 def tag_visible(element):
-            if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
-                return False
-            if isinstance(element, Comment):
-                return False
-            return True
+        if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+            return False
+        if isinstance(element, Comment):
+            return False
+        return True
 
-def text_from_html(body):
-    soup = BeautifulSoup(body, 'html.parser')
-    texts = soup.findAll(text=True)
-    visible_texts = filter(tag_visible, texts)
-    return u" ".join(t.strip() for t in visible_texts)
+list_of_info = [
+    'OrgFullName',
+    "OfficialTitle", #title of study
+    "StatusVerifiedDate",
+    "OverallStatus",
+    "StartDate",
+    "CompletionDate",
+    "BriefSummary",
+    "Condition",
+    "ArmGroupDescription",
+    "ArmGroupInterventionName", #drug
+    "PrimaryOutcomeDescription",
+    "SecondaryOutcomeDescription",
+    "OtherOutcomeMeasure",
+    "OtherOutcomeDescription",
+    "OtherOutcomeTimeFrame",
+    "OtherOutcomeDescription", #length of stay
+    "EligibilityCriteria", #participants
+    "FlowGroupDescription",
+    "FlowGroupDescription",
+    "OutcomeMeasureTitle",
+    "OutcomeMeasureDescription",
+    "OutcomeMeasureTimeFrame",
+    "OutcomeGroupDescription",
+    "OutcomeMeasureDescription",
+    "OutcomeMeasureTimeFrame",
+]
+
+def xml_to_df(data):
+    lst_info = []
+
+    req = Request(
+        url=data, #this is where the variable needs to go
+        headers={'User-Agent': 'Mozilla/5.0'}
+    )
     
-# Set the User-Agent header to mimic a web browser
-req = Request(
-    url='https://www.zacks.com/stock/news/2177620/pacira-pcrx-q3-earnings-and-revenues-miss-23-view-updated', #this is where the variable needs to go
-    headers={'User-Agent': 'Mozilla/5.0'}
-)
-webpage = urlopen(req).read()
-article_str = text_from_html(webpage) #string from the entire webpage
-# print(article_str)
-from nltk.tokenize import sent_tokenize
+    webpage = urlopen(req).read()
+    webpage = webpage.decode('utf-8')
+
+    for i in list_of_info:
+        string = i #this is the headers that I need
+        num_of_start = len(string) #length of header to get end of string
+        start_idx = webpage.find(string) #find header
+        end_idx = webpage.find("</Field>", start_idx) 
+        lst_info.append(webpage[start_idx + num_of_start:end_idx])
+
+    df = pd.DataFrame(columns=['Characteristics', 'Info'])
+    df['Characteristics'] = list_of_info
+    df['Info'] = lst_info
+    return df
+
+for i in lst:
+    print(xml_to_df(i))
 
 
-sentences = sent_tokenize(article_str)
+#from nltk.tokenize import sent_tokenize
+
+
+#sentences = sent_tokenize(article_str)
 # print(sentences)
 # print(sentiment(sentences))
-for sentence in sentences:
-    if len(sentence)< 512:
-        temp = sentiment(sentence)
-        if temp[0]['label'] == 'Positive':
-            print(sentence,'\n')
+#for sentence in sentences:
+    #if len(sentence)< 512:
+        #temp = sentiment(sentence)
+        #if temp[0]['label'] == 'Positive':
+            #print(sentence,'\n')
 
