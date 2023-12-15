fad9a1620cf2572422f135b97041c83660aa3cbd
Jessica Nguyen <89553284+Jnguye84@users.noreply.github.com>
2023-12-15 16:25:00 -0500

    Update k_means.py

--- virtenv/k_means.py
+++ virtenv/k_means.py
@@ -9,37 +9,27 @@ from sklearn.metrics import silhouette_score
 import kmeans
 import warnings
 warnings.filterwarnings("ignore")
+lst = []
 
 df = pd.read_csv('data.csv')
-X = df[['Positive', 'Negative', 'URL']]
-num = 4
+X = df[['Positive', 'Negative']]
 
-K
-    k_means_optimum = KMeans(n_clusters = num)
-    y = k_means_optimum.fit_predict(X[['Positive', 'Negative']])
+k_means_optimum = KMeans(n_clusters = 4)
+y = k_means_optimum.fit_predict(X[['Positive', 'Negative']])
+cluster_centers_ = k_means_optimum.cluster_centers_
 
-    # Find the most important cluster
-    cluster_sizes = np.unique(y, return_counts=True)[1]
-    most_important_cluster = np.argmax(cluster_sizes)
+# Find the most important cluster
+cluster_sizes = np.unique(y, return_counts=True)[1]
+most_important_cluster = np.argmax(cluster_sizes)
 
-    df['cluster'] = y  
-    df1 = df[df.cluster==0]
-    df2 = df[df.cluster==1]
-    df3 = df[df.cluster == 2]
-    df4 = df[df.cluster == 3]
+df['cluster'] = y  
+df1 = df[df.cluster==0]
+df2 = df[df.cluster==1]
+df3 = df[df.cluster == 2]
+df4 = df[df.cluster == 3]
 
-    # Calculate the Silhouette score
-    s = silhouette_score(X, y)
-
-    return s, df1, df2, df3, df4, most_important_cluster, cluster_centers
-
-for num in range (2,10):
-    lst.append(KCluster(num, X, df)[0]) #lst of silhouette scores
-
-idx = lst.index(max(lst)) #finding number of optimal clusters
-
-#plot
-s, df1, df2, df3, df4, most_important_cluster, cluster_centers = KCluster(idx, X, df)
+# Calculate the Silhouette score
+s = silhouette_score(X, y)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -52,9 +42,9 @@ ax.scatter(df4['Positive'] , df4['Negative'])
 u_labels = np.unique(df.cluster)
 
 print('The value of the center of this cluster is    ' '[Positive, Negative]')
-print('                                          ', cluster_centers[most_important_cluster])
+print('                                          ', cluster_centers_[most_important_cluster])
 
-j = np.argmax(abs(cluster_centers[most_important_cluster]))
+j = np.argmax(abs(cluster_centers_[most_important_cluster]))
 if j == 0:
     print('Sentiment from Finbert is classified as Positive')
 else:
