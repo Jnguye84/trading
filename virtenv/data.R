294bc63d2c55ee80496b8a1e18d7e2241fa5613f
Jessica Nguyen <89553284+Jnguye84@users.noreply.github.com>
2023-12-22 15:41:58 -0500

    html site works

--- virtenv/data.R
+++ virtenv/data.R
@@ -1,21 +1,26 @@
+#! /usr/bin/Rscript
 #install.packages("shiny", type="binary")
 library(devtools)
 #install_github("sachsmc/rclinicaltrials")
-#nctids <- c('NCT01801124')
-drug <- 'Exparel'
-#drug <- read_csv('drug.csv')
-#list_ids <- read_csv('my_NCTIds.csv')
 library(rclinicaltrials)
 
+results_sentiment <- function(drug){ #for extracting numbers and sentiment
+  y <- clinicaltrials_download(query = c(paste('cond=',drug,sep=''), 'rslt=With'), count = 10, include_results = TRUE)
+  result <- y$study_information$outcomes
+}
+
+results_participants <- function(drug){ #to see how many unfinished studies a drug has, how many had unsuccessful participants
+  y <- clinicaltrials_download(query = c(paste('cond=',drug,sep=''), 'rslt=With'), count = 10, include_results = TRUE)
+  summary(y$study_results$participant_flow)
+}
+
+results_somenumber <- function(drug, portsize){
+    print(portsize)
+}
+
+
+
 #They are organized into study information, locations, outcomes, interventions, results, and textblocks. Results, where available, is itself a list with three dataframes: participant flow, baseline data, and outcome data.
-
-#for nctid in nctids{
-    #y <- clinicaltrials_download(query = c(paste('cond=',drug,sep=''), 'rslt=With', paste('term=',nctid,sep='')), count = #10, include_results = TRUE)
-#}
-
-#summary(y$study_results$baseline_data)
-
-y <- clinicaltrials_download(query = c(paste('cond=',drug,sep=''), 'rslt=With'), count = 10, include_results = TRUE)
 
 #term -> Search Terms
 #recr -> Recruitment
@@ -31,5 +36,3 @@ y <- clinicaltrials_download(query = c(paste('cond=',drug,sep=''), 'rslt=With'),
 #> names(y$study_results)
 #[1] "participant_flow" "baseline_data"    "outcome_data"    
 
-
-#useful for finding outcomes: y$study_information$outcomes for extracting numbers and sentiment, y$study_results$participant_flow is good to see how many unfinished studies a drug has, how many had unsuccessful participants
