34e1a7c34dcdb107ec9d15b66dc48ae3cd25d350
Jessica Nguyen <89553284+Jnguye84@users.noreply.github.com>
2024-01-04 16:57:18 -0500

    Update extract.r

--- virtenv/Financial analysis/SNG data/extract.r
+++ virtenv/Financial analysis/SNG data/extract.r
@@ -5,16 +5,26 @@ truncated <- read.csv('/Users/jessicanguyen/Documents/GitHub/trading/virtenv/Fin
 split_data <- strsplit(as.character(truncated), "\\}", fixed=TRUE)
 
 ticker <- sapply(split_data, function(x) trimws(gsub("\"", "", x[1])))[1]
-tickers <- sapply(split_data, function(x) trimws(gsub("c\\(|\\)", "", x)))[1]
-ticker <- strsplit(ticker, ",")[[1]]
+tickers <- sapply(split_data, function(x) trimws(gsub("[\\\\\"]", "", x)))[1]
+tickers <- trimws(strsplit(tickers, ",")[[1]])
+tickers <- gsub("^c\\(|\\)$", "", tickers)
+#tickers done
+test_lst <- c('SEC', 'DOJ') #this is where all the tickers should be
 
 companies <- sapply(split_data, function(x) trimws(gsub("\"", "", x[1])))[2]
-
 companies_list <- strsplit(as.character(companies), "\\}")
 companies_list <- lapply(companies_list, function(x) as.list(strsplit(x, ",")))
-for (i in seq_along(companies_list)) {
-  companies_list[[i]] <- lapply(companies_list[[i]], function(x) trimws(gsub("[^a-zA-Z0-9 ]", "",x)))
-  #delete elements within a list that are not in another list (list of tickers)
-  #companies_list[[i]] <- companies_list[[i]][companies_list[[i]] %in% ticker]
-}
+companies_list[[1]] <- lapply(companies_list[[1]], function(x) trimws(gsub("[^a-zA-Z0-9 ]", "",x)))
 
+for (i in seq_along(companies_list)) {
+  for (j in seq_along(companies_list[[i]])){
+    for (k in seq_along(companies_list[[i]][[j]])){
+      if (companies_list[[i]][[j]][[k]] %in% test_lst == TRUE){
+        companies_list[[i]][[j]][[k]] <- companies_list[[i]][[j]][[k]]
+      }
+      else {
+         companies_list[[i]][[j]][[k]] <- NA
+      }
+    }
+  }
+}
