30fd65a6f497b20c8e81dd47eee47b5330c8f15a
Jessica Nguyen <89553284+Jnguye84@users.noreply.github.com>
2024-01-05 20:25:35 -0500

    dataframe made extract.R

--- virtenv/Financial analysis/SNG data/extract.r
+++ virtenv/Financial analysis/SNG data/extract.r
@@ -1,8 +1,11 @@
 library(tidyverse)
-data <- read.csv('/Users/jessicanguyen/Documents/GitHub/trading/outputTemp.csv', header=FALSE)
-extra_companies <- readLines('companyNames.txt', warn = FALSE)
-extra_companies <- lapply(extra_companies, function(line) strsplit(line, ",")[[1]])
-extra_companies <- gsub("'", "", extra_companies)
+library(tidyr)
+library(purrr)
+data <- read.csv("/Users/jessicanguyen/Documents/GitHub/trading/outputTemp.csv", header=FALSE)
+extra_companies <- readLines("companyNames.txt", warn = FALSE)
+extra_companies <- sapply(extra_companies, function(line) strsplit(line, ","))
+extra_companies <- sapply(extra_companies, function(x) trimws(gsub("\'", "", x)))
+extra_companies <- as.list(extra_companies)
 
 split_data <- strsplit(as.character(data), "\\}", fixed=TRUE)
 
@@ -10,6 +13,7 @@ ticker <- sapply(split_data, function(x) trimws(gsub("\"", "", x[1])))[1]
 tickers <- sapply(split_data, function(x) trimws(gsub("[\\\\\"]", "", x)))[1]
 tickers <- trimws(strsplit(tickers, ",")[[1]])
 tickers <- as.list(gsub("^c\\(|\\)$", "", tickers))
+combined_tickers <- c(tickers, extra_companies, "Mirati Therapeutics", "Jazz Pharmaceuticals")
 #tickers done
 
 companies <- sapply(split_data, function(x) trimws(gsub("\"", "", x[1])))[2]
@@ -20,7 +24,7 @@ companies_list[[1]] <- lapply(companies_list[[1]], function(x) trimws(gsub("[^a-
 for (i in seq_along(companies_list)) {
   for (j in seq_along(companies_list[[i]])){
     for (k in seq_along(companies_list[[i]][[j]])){
-      if (companies_list[[i]][[j]][[k]] %in% tickers == TRUE){
+      if (companies_list[[i]][[j]][[k]] %in% combined_tickers == TRUE){
         companies_list[[i]][[j]][[k]] <- companies_list[[i]][[j]][[k]]
       }
       else {
@@ -32,4 +36,8 @@ for (i in seq_along(companies_list)) {
 
 cleaned_list <- lapply(companies_list[[1]], function(sublist) sublist[!is.na(sublist)])
 cleaned_list <- cleaned_list[-length(cleaned_list)]
+cleaned_list <- lapply(cleaned_list, function(x) if (length(x) == 0) "" else x)
+
 sna <- data.frame(Tickers= as.character(tickers), Relationships = as.character(cleaned_list))
+sna <- unnest(sna, cols = Relationships)
+df_unnested <- separate(sna, col = Relationships, into = c("col1", "col2", "col3", "col4"), sep = ",")
